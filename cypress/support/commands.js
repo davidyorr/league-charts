6758363e92a43e46f88103166b6d805ab634613a
David Orr <davidyorr@gmail.com>
2020-10-19 04:19:34 -0400

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,6 +3,5 @@ import { addMatchImageSnapshotCommand } from "cypress-image-snapshot/command";
 
 addMatchImageSnapshotCommand({
   // the font is causing there to be slight differences, so increase this slightly
-  failureThreshold: 0.5,
-  customSnapshotsDir: "cypress/customdir",
+  failureThreshold: 0.15,
 });
