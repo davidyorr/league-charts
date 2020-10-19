f2c605c9e337496f2975dfa1a77be0ed0aa1a6c6
David Orr <davidyorr@gmail.com>
2020-10-19 04:43:58 -0400

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,7 +3,7 @@ import { addMatchImageSnapshotCommand } from "cypress-image-snapshot/command";
 
 addMatchImageSnapshotCommand({
   // the font is causing there to be slight differences, so increase this slightly
-  failureThreshold: 0.1,
+  failureThreshold: 0.05,
   failureThresholdType: "percent",
   customDiffConfig: {
     threshold: 0.1,
